366d2db5d50544b4c8019ce37f00bbe53a979596
sycured <60801403+sycured@users.noreply.github.com>
2020-02-09 13:59:32 -0500

    fix import + little refacto with f-style and replace duplicate by a function

--- confgen.py
+++ confgen.py
@@ -1,28 +1,36 @@
 from __future__ import print_function
-import subprocess
-import time
+
+from time import sleep
+from subprocess import check_output, PIPE
+
 
 # python 2
 # Usage: with libimobiledevice installed, plug in device to generate
 # ini entry for autotss
 
+
+def deviceinfo(key, line):
+    return check_output(['ideviceinfo', '-k', key, '-u', line],
+                        stderr=PIPE).rstrip()
+
+
 UDIDs = []
 print("Waiting for devices...\n")
 while 1:
-	status = subprocess.check_output(['idevice_id', '-l'], stderr=subprocess.PIPE).rstrip().split('\n') #rstrip because \n
-	if status[0]:
-		for line in status:
-			if line not in UDIDs:
-				device_name     = subprocess.check_output(['ideviceinfo', '-k', 'DeviceName',    '-u', line], stderr=subprocess.PIPE).rstrip() #rstrip because \n
-				product_type    = subprocess.check_output(['ideviceinfo', '-k', 'ProductType',   '-u', line], stderr=subprocess.PIPE).rstrip() #rstrip because \n
-				ecid            = subprocess.check_output(['ideviceinfo', '-k', 'UniqueChipID',  '-u', line], stderr=subprocess.PIPE).rstrip() #rstrip because \n
-				hardware_model  = subprocess.check_output(['ideviceinfo', '-k', 'HardwareModel', '-u', line], stderr=subprocess.PIPE).rstrip() #rstrip because \n
-				
-				print("[" + device_name + "]")
-				print("identifier = " + product_type)
-				print("ecid = " + ecid)
-				print("boardconfig = " + hardware_model + "\n")
+    status = check_output(['idevice_id', '-l'],
+                          stderr=PIPE).rstrip().split('\n')
+    if status[0]:
+        for line in status:
+            if line not in UDIDs:
+                device_name = deviceinfo('DeviceName', line)
+                product_type = deviceinfo('ProductType', line)
+                ecid = deviceinfo('UniqueChipID', line)
+                hardware_model = deviceinfo('HardwareModel', line)
 
-				UDIDs.append(line)
+                print(f'[ {device_name} ]\n'
+                      f'identifier = {product_type}\n'
+                      f'ecid = {ecid}\n'
+                      f'boardconfig = {hardware_model}\n')
+                UDIDs.append(line)
 
-	time.sleep(1)
+    sleep(1)
